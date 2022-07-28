8d6bcf0f1fefccb1b032fea25c7dfa8cfd473aed
Reham-Fahmy <64305358+Reham-Fahmy@users.noreply.github.com>
2022-07-29 01:11:47 +0200

    Update first-boot.sh

--- 01-sample-instance/scripts/first-boot.sh
+++ 01-sample-instance/scripts/first-boot.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-
-echo "hello world"
+# Use this for your user data (script from top to bottom)
+# install httpd (Linux 2 version)
+yum install -y wget
+wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+yum install -y fontconfig java-11-openjdk
+yum install jenkins
+yum start jenkins
+yum enable jenkins
