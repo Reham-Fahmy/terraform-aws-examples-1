5fa08c630eed3ff72060769980e1417ebab34dcf
Reham-Fahmy <64305358+Reham-Fahmy@users.noreply.github.com>
2022-07-28 22:23:34 +0200

    Update first-boot.sh

--- 02-instance-with-volume/scripts/first-boot.sh
+++ 02-instance-with-volume/scripts/first-boot.sh
@@ -1,3 +1,5 @@
-#!/bin/bash
-
-echo "hello world"
+yum update -y
+yum install -y httpd
+systemctl start httpd
+systemctl enable httpd
+echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
